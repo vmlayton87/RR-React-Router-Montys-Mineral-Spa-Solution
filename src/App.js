44e9a61778d450141821d4b740ffd71ef0db8504
vmlayton87 <vmlayton87@gmail.com>
2024-02-27 19:26:34 -0500

    base bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-
 import './App.css';
-
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import { Nav, Container } from 'react-bootstrap';
+
 
 
 function App() {
@@ -17,19 +17,26 @@ function App() {
       <header>
         <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-        <div className="navBar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About Us</Link>
-            </li>
-            <li>
-              <Link to="/packages">Our Packages</Link>
-            </li>
-          </ul>
-        </div>
+        <Container>
+    <Nav defaultActiveKey="/" variant="tabs" fill>
+        <Nav.Item>
+            <Nav.Link href="/"> 
+                <Link to="/">Home</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"aboutPage"}> 
+                <Link to="/about">About Us</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"packagesPage"}> 
+                <Link to="/packages">Our Packages</Link> 
+            </Nav.Link>
+         </Nav.Item>
+    </Nav>
+</Container>
+
 
       </header>
       <div className="display">
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,28 @@
-
+import { Card, Container, ListGroup } from 'react-bootstrap'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) =>{
+    return (
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>
+        )
+    })
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
                     {displayPackages}
-                </ul>
-            </div>
-        </div>
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
+
+
+
